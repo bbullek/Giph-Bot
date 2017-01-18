f97db1eef65d68e638906ba24135646c90710fdb
bbullek <bbullek@live.com>
2017-01-17 22:39:47 -0500

    Restructured node files to be in root directory

---
+++ config.js
@@ -0,0 +1,8 @@
+/* Contains secret tokens for Twitter authentication. */
+
+module.exports = {
+	consumer_key: 	     "8M1djWmPuertoqZNV55UstR47" 
+  , consumer_secret:     "RKGACCnLX8bVPljBoIvTXQmHazjsGB4z1NkVpxXYhzSzj2ADEu"
+  , access_token:        "816006955739254786-cu0sIGSBcVsQ7UDQpqn5czvZ04Y3Oyd"
+  , access_token_secret: "3WvXGn83fchmS8HDYKw76DpIE6fw1ESSOzLsiaModb6h1"
+}
